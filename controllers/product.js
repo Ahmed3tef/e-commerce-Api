3652d131194f20ba89f9951b417d5d33c5ce7d0a
Ahmed Atef <atteff101@gmail.com>
2022-12-29 00:10:18 +0200

    update

--- controllers/product.js
+++ controllers/product.js
@@ -4,64 +4,78 @@ import slugify from 'slugify';
 import asyncHandler from 'express-async-handler';
 import ApiError from '../utils/ApiError.js';
 import { ProductModel } from '../models/product.js';
+import ApiFeatures from '../utils/ApiFeatures.js';
 
 export const getProducts = asyncHandler(async (req, res, next) => {
-  // 1- filtering
-  const queryObject = { ...req.query };
-  const excludesFields = ['sort', 'limit', 'page', 'fields'];
-  excludesFields.forEach(field => delete queryObject[field]);
+  // // 1- filtering
+  // const queryObject = { ...req.query };
+  // const excludesFields = ['sort', 'limit', 'page', 'fields'];
+  // excludesFields.forEach(field => delete queryObject[field]);
 
-  // apply filtering with less or greater
-  // هنا انا بقوله لو جه ف الركوست كويري القيم دي المفروض هي جاية مفهاش ال$  ف انا هحطها عشان انا محتاجها ف مونجو هي اللي هتفلتر الحاجة ليا
-  // هنحوله من اوبجكت ل سترنج عشان نقدر نتعامل معاه وبعد كدا هنعمل عليه عملية اضافة الرمز $ وبعدين هنحوله تاني ل اوبجكت ونمرره للفايند وبكدا هنكون فلترنا
+  // // apply filtering with less or greater
+  // // هنا انا بقوله لو جه ف الركوست كويري القيم دي المفروض هي جاية مفهاش ال$  ف انا هحطها عشان انا محتاجها ف مونجو هي اللي هتفلتر الحاجة ليا
+  // // هنحوله من اوبجكت ل سترنج عشان نقدر نتعامل معاه وبعد كدا هنعمل عليه عملية اضافة الرمز $ وبعدين هنحوله تاني ل اوبجكت ونمرره للفايند وبكدا هنكون فلترنا
 
-  let queryStr = JSON.stringify(queryObject).replace(
-    /\b(gt|gte|lt|lte)\b/g,
-    match => `$${match}`
-  );
-  // 2- pagination
-  const page = +req.query.page || 1;
-  const limit = +req.query.limit || 5;
-  const skip = (page - 1) * limit;
+  // let queryStr = JSON.stringify(queryObject).replace(
+  //   /\b(gt|gte|lt|lte)\b/g,
+  //   match => `$${match}`
+  // );
+  // // 2- pagination
+  // const page = +req.query.page || 1;
+  // const limit = +req.query.limit || 5;
+  // const skip = (page - 1) * limit;
 
-  let mongooseQuery = ProductModel.find(JSON.parse(queryStr))
-    .skip(skip)
-    .limit(limit)
-    .populate(['category', 'subcategory', 'brand']);
+  // let mongooseQuery = ProductModel.find(JSON.parse(queryStr))
+  //   .skip(skip)
+  //   .limit(limit)
+  //   .populate(['category', 'subcategory', 'brand']);
 
-  // 3- sorting
-  if (req.query.sort) {
-    const sortBy = req.query.sort.split(',').join(' ');
-    mongooseQuery = mongooseQuery.sort(sortBy);
-  }
+  // // 3- sorting
+  // if (req.query.sort) {
+  //   const sortBy = req.query.sort.split(',').join(' ');
+  //   mongooseQuery = mongooseQuery.sort(sortBy);
+  // }
 
-  // 4- select fields:
-  if (req.query.fields) {
-    const fields = req.query.fields.split(',').join(' ');
-    mongooseQuery = mongooseQuery.select(fields);
-  } else {
-    mongooseQuery = mongooseQuery.select('-__v');
-  }
+  // // 4- select fields:
+  // if (req.query.fields) {
+  //   const fields = req.query.fields.split(',').join(' ');
+  //   mongooseQuery = mongooseQuery.select(fields);
+  // } else {
+  //   mongooseQuery = mongooseQuery.select('-__v');
+  // }
 
-  // 5- search
-  if (req.query.keyword) {
-    // هيكون جايلي كلام معين انا عايز اخده وادور ف الاسم والوصف بتاع المنتج
-    const query = {};
-    query.$or = [
-      {
-        // options  i بتتعامل مع الكلام الصغير زي الكبير small and capital equals
-        title: { $regex: req.query.keyword, options: 'i' },
-      },
-      {
-        description: { $regex: req.query.keyword, options: 'i' },
-      },
-    ];
-    // كل حاجة انت عايز تدور فيها هتديها اوبجكت واسم الفيلد و الاوبشنين دول
+  // // 5- search
+  // if (req.query.keyword) {
+  //   // هيكون جايلي كلام معين انا عايز اخده وادور ف الاسم والوصف بتاع المنتج
+  //   const query = {};
+  //   query.$or = [
+  //     {
+  //       // options  i بتتعامل مع الكلام الصغير زي الكبير small and capital equals
+  //       title: { $regex: req.query.keyword, options: 'i' },
+  //     },
+  //     {
+  //       description: { $regex: req.query.keyword, options: 'i' },
+  //     },
+  //   ];
+  //   // كل حاجة انت عايز تدور فيها هتديها اوبجكت واسم الفيلد و الاوبشنين دول
 
-    mongooseQuery = mongooseQuery.find(query);
-  }
+  //   mongooseQuery = mongooseQuery.find(query);
+  // }
 
-  const products = await mongooseQuery;
+  // بدل الكلام اللي فوق دا كله احنا اخدناه ف كلاس عشان نقدر نستخدمه ف اكتر من مكان تاني ودا بقا شكل الكود الجديد
+
+  const apiFeatures = new ApiFeatures(ProductModel.find(), req.query)
+    .paginate()
+    .sort()
+    .search()
+    .filter()
+    .limitFields();
+
+  const products = await apiFeatures.mongooseQuery.populate([
+    'category',
+    'subcategory',
+    'brand',
+  ]);
 
   res.status(200).json({ status: 'success', data: products });
 });
